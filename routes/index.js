2b47ac8506a32f975aac7e3f6d4d1b45db5d0837
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-11-15 23:28:31 +0200

    test1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-    res.json({msg:"app work 123"})
+    res.json({msg:"app work "})
 })
 
 module.exports = router;
